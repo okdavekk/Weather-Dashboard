885b521dfd5565df919ed69816f088fc41d6e2d5
okdavekk <okdavekk@gmail.com>
2022-06-01 18:10:08 -0400

    After instructor template transciption.

--- style.js
+++ style.js
@@ -1,23 +1,27 @@
+var APIKey = "6ca2b4a417c131dfd2fadd3234e442ea";
+var geoAPI = "http://api.openweathermap.org/geo/1.0/direct?q=Charlotte&limit=5&appid=6ca2b4a417c131dfd2fadd3234e442ea"
+// var oneCallAPI = "`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=6ca2b4a417c131dfd2fadd3234e442ea`"
+
 function init() {
-    fetch("https://api.github.com/orgs/twitter/public_members?per_page=5")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log("Twitter Public Members: Raw data \n----------");
-            for (var member of data) getUserRepos(member.login);
-        });
+    fetch(geoAPI)
+    .then(function (response) {
+        return response.json();
+    }) 
+    .then(function(data) {
+        console.log(data[0].lat, data[0].lon);
+        getOneCallWeather(data[0].lat, data[0].lon);
+    });
 }
 
-function getUserRepos(username) {
-    fetch("https://api.github.com/users/" + username + "/repos?per_page=5")
-        .then(function (response) {
+
+function getOneCallWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=6ca2b4a417c131dfd2fadd3234e442ea&units=imperial`)
+    .then(function (response) {
         return response.json();
+    }) 
+    .then(function(data) {
+        console.log(data);
     })
-        .then(function (data) {
-            console.log("Twitter Repositories: Names only \n----------");
-            console.log(data)
-        });
 }
 
 init();
